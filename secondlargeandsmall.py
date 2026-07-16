a44dc410844639018dfe017dd8cdbb2a94faa926
SC-Vimalathithyan <sc.vimalathithyan@gmail.com>
2026-07-16 10:12:28 +0530

    Second small and largest in array

---
+++ secondlargeandsmall.py
@@ -0,0 +1,15 @@
+def Secondlargeandsmall(arr):
+    if len(arr)==0:
+        return None
+    else:
+        for i in range(len(arr)):
+            for j in range(len(arr)):
+                if(arr[i]<arr[j]):
+                    temp=arr[i]
+                    arr[i]=arr[j]
+                    arr[j]=temp
+    return arr
+
+arr=[1,9,2,4,6,10,8,7]
+Secondlargeandsmall(arr)
+print(arr[-2],arr[1])
